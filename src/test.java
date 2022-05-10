17f9e4f189da29410e31a6b4a9f66cf118d080f8
carchiav <57341597+carchiav@users.noreply.github.com>
2022-05-10 12:58:54 -0700

    Add files via upload

---
+++ src/test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class test {
+    public static void main(String[] Args){
+        Scanner input = new Scanner (System.in);
+        System.out.println("Input some text:");
+        String stuff = input.nextLine();
+        System.out.println(stuff);
+        jkagdfkajhgdkahfkajhgsdfkjaghfkajshgdfkjashgdfkjashgfkajsgfaksjgfkajgh
+    }
+}
---
+++ src/tests2w.java
@@ -0,0 +1,5 @@
+public class tests2w {
+    public static void main(){
+
+    }
+}
