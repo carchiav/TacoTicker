1ef2d41a4d2f0160694cbfbadab91e00b878125a
carchiav <57341597+carchiav@users.noreply.github.com>
2022-05-10 19:01:39 -0700

    Update Echo2x.java

--- src/Echo2x.java
+++ src/Echo2x.java
@@ -4,8 +4,6 @@ public class Echo2x {
         Scanner input = new Scanner (System.in);
         System.out.println("Input some text:");
         String stuff = input.nextLine();
-        System.out.println(stuff);
-        System.out.println(stuff);
 	  System.out.println("teft");
     }
 }
