ff8ad27ecd8685e69dceec350890489e03096756
carchiav <cachiavatti@gmail.com>
2022-05-11 18:34:47 -0700

    Working Test Files for Experimenting

--- src/Graphicstest.java
+++ src/Graphicstest.java
@@ -1,5 +1,4 @@
 import java.awt.*;       // Using AWT's Graphics and Color
-import java.awt.event.*; // Using AWT event classes and listener interfaces
 import javax.swing.*;    // Using Swing's components and containers
 
 /** Custom Drawing Code Template */
@@ -9,13 +8,11 @@ public class Graphicstest extends JFrame {
     public static final int CANVAS_WIDTH  = 640;
     public static final int CANVAS_HEIGHT = 480;
 
-    // Declare an instance of the drawing canvas,
-    // which is an inner class called DrawCanvas extending javax.swing.JPanel.
-    private DrawCanvas canvas;
-
     // Constructor to set up the GUI components and event handlers
     public Graphicstest() {
-        canvas = new DrawCanvas();    // Construct the drawing canvas
+        // Declare an instance of the drawing canvas,
+        // which is an inner class called DrawCanvas extending javax.swing.JPanel.
+        DrawCanvas canvas = new DrawCanvas();    // Construct the drawing canvas
         canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
 
         // Set the Drawing JPanel as the JFrame's content-pane
@@ -32,7 +29,7 @@ public class Graphicstest extends JFrame {
     /**
      * Define inner class DrawCanvas, which is a JPanel used for custom drawing.
      */
-    private class DrawCanvas extends JPanel {
+    private static class DrawCanvas extends JPanel {
         // Override paintComponent to perform your own painting
         @Override
         public void paintComponent(Graphics g) {
@@ -55,14 +52,14 @@ public class Graphicstest extends JFrame {
         }
     }
 
-    /*The entry main method
+    //The entry main method
     public static void main(String[] args) {
         // Run the GUI codes on the Event-Dispatching thread for thread safety
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new CGGraphicstest(); // Let the constructor do the job
+                new Graphicstest(); // Let the constructor do the job
             }
         });
-    }*/
+    }
 }
