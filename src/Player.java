c379911ec6ac25d0f93cda98114b0b552e3ca79e
Carson <cachiavatti@gmail.com>
2022-05-12 12:26:33 -0700

    test

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,42 @@
+import java.util.ArrayList;
 public class Player {
+    int tacoCount;
+    int tacosPerTick;
+    int TPS;
+
+    int[] producerCounts;
+    Arraylist<Producer> producers;
+    int[] upgrades;
+
+    public Player(){
+        public void loadGame();
+    }
+    public void calcTPS(){
+
+    }
+    public void calcTPSLoop(){
+        long start = System.currentTimeMillis();
+        int i = 0;
+        do{
+
+        }while(i!=1)
+
+    }
+    public void removeTacos(int i){
+
+    }
+    public void addTacos(int i){
+
+    }
+    public void saveGame(){
+
+    }
+    public void loadGame(){
+        tacoCount = 0;
+        tacosPerTick = 1;
+        TPS = 0;
+        producerCounts = new int[7];
+        upgrades = new int[9];
+    }
+
 }
--- src/TacoTickerDriver.java
+++ src/TacoTickerDriver.java
@@ -1,2 +1,5 @@
 public class TacoTickerDriver {
+    public static void main(){
+
+    }
 }
