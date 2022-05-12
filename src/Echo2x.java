489cd0a4bde39fd1ed37b081e0c02b0df19b66dd
Carson <cachiavatti@gmail.com>
2022-05-12 11:58:25 -0700

    test

--- src/Echo2x.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Echo2x {
-    public static void main(String[] Args){
-        Scanner input = new Scanner (System.in);
-        System.out.println("Input some text:");
-        String stuff = input.nextLine();
-        System.out.println(stuff);
-    }
-}
