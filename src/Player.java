745e99b6e76d34a28aff8f02aa114bf805278a3a
Carson <cachiavatti@gmail.com>
2022-05-12 13:08:30 -0700

    Working

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,10 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+
 public class Player {
     int tacoCount;
     int tacosPerTick;
@@ -9,14 +15,16 @@ public class Player {
     int[] upgrades;
 
     public Player(){
-        public void loadGame();
+        loadGame();
     }
     public void calcTPS(){
         int temp = 0;
         for (Producer i : producers){
             temp += i.getTPS();
         }
+        temp = 5;
         TPS = temp;
+        System.out.println(temp);
     }
     public void calcTPSLoop() throws InterruptedException {
         int i = 0;
@@ -27,21 +35,37 @@ public class Player {
 
     }
     public void removeTacos(int i){
-
+        tacoCount -= i;
     }
     public void addTacos(int i){
-
+        tacoCount += i;
     }
-    public void saveGame(){
-
+    public void saveGame() {
+        File saveFile = new File("SaveData.txt");
+        try {
+            FileWriter writer = new FileWriter("SaveData.txt");
+            writer.write(tacoCount + "|" + tacosPerTick + "|" + TPS + "|" + producerCounts+"|" + upgrades);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("error");
+        }
     }
-    public void loadGame(){
-        tacoCount = 0;
-        tacosPerTick = 1;
-        TPS = 0;
-        producerCounts = new int[7];
-        upgrades = new int[9];
-        producers = new ArrayList<Producer>();
+        public void loadGame(){
+        try {
+            File save = new File("saveData.txt");
+            Scanner input = new Scanner(save);
+            String curr = input.nextLine();
+            tacoCount = Integer.parseInt(curr);
+
+            }
+        catch(FileNotFoundException e){
+            tacoCount = 0;
+            tacosPerTick = 1;
+            TPS = 0;
+            producerCounts = new int[7];
+            upgrades = new int[9];
+            producers = new ArrayList<Producer>();
+        }
     }
 
 }
--- src/TacoTickerDriver.java
+++ src/TacoTickerDriver.java
@@ -1,5 +1,9 @@
 public class TacoTickerDriver {
-    public static void main(){
+    public static void main(String[] args) throws InterruptedException {
+        Player Player1 = new Player();
+        Player1.saveGame();
+        Player1.calcTPSLoop();
+
 
     }
 }
