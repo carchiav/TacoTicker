ef5e002a2931d5224dbfa20f79f8588406b84cf4
carchiav <57341597+carchiav@users.noreply.github.com>
2022-05-11 18:26:24 -0700

    Add files via upload

---
+++ src/Echo2x.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Echo2x {
+    public static void main(String[] Args){
+        Scanner input = new Scanner (System.in);
+        System.out.println("Input some text:");
+        String stuff = input.nextLine();
+        System.out.println(stuff);
+    }
+}
