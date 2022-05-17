f392ddd78bac7d6ff502090312280f24f93c8fdc
Dhilan <bytheseamoonlight@gmail.com>
2022-05-16 19:57:47 -0700

    1

--- src/Buttons.java
+++ src/Buttons.java
@@ -1,2 +1,33 @@
-public class Buttons {
+import javax.swing.*;
+import java.awt.*;
+import java.beans.ConstructorProperties;
+
+public class Buttons extends JButton{
+    private int x;
+    private int y;
+    private JButton screenButton;
+    private String name;
+    public Buttons(String name, int x, int y){
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        screenButton = new JButton(name);
+        setText(name);
+        addActionListener(e -> actiontest());
+        setPreferredSize(new Dimension(50, 50));
+        setActionCommand("Yes");
+        setVisible(true);
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+    @Override
+    public int getY(){
+        return y;
+    }
+    public void actiontest(){
+        System.out.println(name);
+    }
 }
--- src/Frame.java
+++ src/Frame.java
@@ -9,8 +9,8 @@ import java.awt.image.*;
 
 public class Frame extends JFrame{
     Panel s;
-    private int w = 640;
-    private int h = 480;
+    private int w = 1000;
+    private int h = 1000;
     public Frame(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(w, h);
@@ -26,6 +26,7 @@ public class Frame extends JFrame{
         s = new Panel(w, h);
         add(s);
 
+        s.setLayout(null);
 
         Icon taco = new ImageIcon("Tacoimage.jpg");
         JButton tacoClicker = new JButton(taco);
@@ -33,6 +34,10 @@ public class Frame extends JFrame{
         tacoClicker.setActionCommand("yes");
         tacoClicker.setPreferredSize(new Dimension(100, 100));
         s.add(tacoClicker);
+
+        Buttons TacoStand = new Buttons("Taco Stand", 300, 400);
+        s.add(TacoStand);
+        TacoStand.setLocation(getX(), getY());
         setVisible(true);
     }
     public void actiontest(){
