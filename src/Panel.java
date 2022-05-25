7b7bb9ee849fbf87497f2f331ee834cbf433b349
Dhilan <bytheseamoonlight@gmail.com>
2022-05-25 12:28:11 -0700

    q

--- src/Panel.java
+++ src/Panel.java
@@ -1,9 +1,4 @@
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import javax.swing.*;
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
 
 public class Panel extends JPanel{
     private int width;
